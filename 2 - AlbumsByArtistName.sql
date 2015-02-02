2ff391bf90517301f26acd987873f259ed552544
Alastair Smith <github@alastairsmith.me.uk>
2015-02-02 20:36:23 +0000

    Add second demo.
    Use AssertEqualsTable to verify a SPROC's behaviour.

---
+++ 2 - AlbumsByArtistName.sql
@@ -0,0 +1,41 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Alastair Smith>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE AlbumsForArtistByName
+	-- Add the parameters for the stored procedure here
+	@artistName NVARCHAR(120)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT dbo.Album.Title 
+	FROM dbo.Album 
+		INNER JOIN dbo.Artist 
+		ON dbo.Album.ArtistId = dbo.Artist.ArtistId
+	WHERE dbo.Artist.Name = @artistName;
+END
+GO
+
+-- DOES IT WORK??
+EXEC [dbo].[AlbumsForArtistByName] @artistName = N'AC/DC'
+GO
---
+++ 2 - AlbumsByArtistName.test.sql
@@ -0,0 +1,32 @@
+﻿USE [Chinook]
+GO
+/****** Object:  StoredProcedure [MusicTests].[test AlbumsForArtistByName returns the albums for the given artist]    Script Date: 03/02/2015 12:28:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [MusicTests].[test AlbumsForArtistByName returns the albums for the given artist]
+AS
+BEGIN
+	-- Arrange
+	CREATE TABLE #actual (
+		AlbumTitle NVARCHAR(160)
+	);
+
+	-- Act
+	INSERT INTO #actual EXEC dbo.AlbumsForArtistByName @artistName = N'Guns N'' Roses';
+
+	-- Assert
+	CREATE TABLE #expected (
+		AlbumTitle NVARCHAR(160)
+	);
+
+	INSERT INTO #expected (AlbumTitle) VALUES (N'Appetite for Destruction');
+	INSERT INTO #expected (AlbumTitle) VALUES (N'Use Your Illusion I');
+	INSERT INTO #expected (AlbumTitle) VALUES (N'Use Your Illusion II');
+
+	EXEC tSQLt.AssertEqualsTable
+		@Expected = '#expected',
+	    @Actual = '#actual'
+END;
